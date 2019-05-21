57a1af6c54732538fc63450e2af0cb3c0a59872d
Łukasz Szęszoł <lusz@softwaremind.com>
2019-05-21 23:34:17 +0200

    Implemented part of task

--- Lab1/File7.fs
+++ Lab1/File7.fs
@@ -2,6 +2,7 @@
 
 open System
 
+
 let drawTree =
     let width = System.Console.WindowWidth
     let height = System.Console.WindowHeight
@@ -13,20 +14,24 @@ let drawTree =
         Console.WriteLine();
 
     Console.WriteLine("B")
-    for i in 0 .. height do
+    for i in 0 .. height do        
         for j in 0 .. width do
-            Console.Write("*");
-        Console.WriteLine();
-
-
-    Console.WriteLine("C")
-    for i in 0 .. height do
-        for j in 0 .. width do
-            if j >= i then
+            let halfOfWidth = (width /2)
+            let halfQuantityOfStars = ((2 * i) /2)
+            if ( j > (halfOfWidth - halfQuantityOfStars)  && j < (halfOfWidth + halfQuantityOfStars)) then
                 Console.Write("*");
-            else 
-                Console.Write("");
+            else Console.Write(" ");
         Console.WriteLine();
+
+    //Console.WriteLine("C")
+    //for i in 0 .. height do
+    //    for j in 0 .. width do
+    //        if j >= i then
+    //            if (j % 2 = 0) then
+    //             Console.Write("*");
+    //        else 
+    //            Console.Write("");
+    //    Console.WriteLine();
 
     
 
