2097a7ec0675216def0050c5cea0f3a6ff43c26b
Łukasz Szęszoł <lusz@softwaremind.com>
2019-05-21 10:56:45 +0200

    Fixed conditions, changed variable name

--- Lab1/File10.fs
+++ Lab1/File10.fs
@@ -1,13 +1,13 @@
 ﻿module File10
 
-let rec func (x : int) =
+let rec func (k : int) =
     
-    if (x < 0) then
+    if (k < 0) then
         false
-    elif (x / 4 >= 0 && x % 1 = 0 ) then
+    elif (k / 4 > 0 && k % 1 = 0 ) then
         true
-    elif (x / 4 >= 0 && x % 3 = 0 ) then
+    elif (k / 4 > 0 && k % 3 = 0 ) then
         false
-    elif (x / 2 = 0 && x % 2 = 0 ) then
-        func (x / 2)
+    elif (k / 2 = 1 && k % 2 = 0 ) then
+        func (k * 2)
     else false
